ed143985fa2c1495152b417da756392e2099b138
Justin Yoo <justin.yoo@aliencube.com>
2017-02-23 03:07:27 +1100

    Update web app

--- src/VueJsTypeScriptAspNetCoreSample/src/main.js
+++ src/VueJsTypeScriptAspNetCoreSample/src/main.js
@@ -4,9 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
-import VueAxios from 'vue-axios'
 
-Vue.use(VueAxios, axios)
+Vue.prototype.axios = axios
 
 /* eslint-disable no-new */
 new Vue({
--- src/VueJsTypeScriptAspNetCoreSample/webpack.config.js
+++ src/VueJsTypeScriptAspNetCoreSample/webpack.config.js
@@ -1,8 +1,7 @@
-﻿"use strict";
+"use strict";
 
 var isDevBuild = process.argv.indexOf('--env.prod') < 0;
 var devConfig = require("./build/webpack.dev.conf");
 var prodConfig = require("./build/webpack.prod.conf");
 
 module.exports = isDevBuild ? devConfig : prodConfig;
-
