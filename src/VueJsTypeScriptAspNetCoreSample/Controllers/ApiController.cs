6fb6620ca6b09badfe1a1f7ab04b1142789f38e9
Justin Yoo <justin.yoo@aliencube.com>
2017-02-22 16:54:12 +1100

    Add another Vue.js app running on ASP.NET Core
    - Replaced vue-resource with axios

---
+++ src/VueJsTypeScriptAspNetCoreSample/Controllers/ApiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VueJsTypeScriptAspNetCoreSample.Controllers
+{
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        [Route("hello")]
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            var msg = new { Message = "Hello World" };
+            return this.Ok(msg);
+        }
+    }
+}
