4445ba93ee7cba5271fc4889286911f3e42bcaa3
Justin Yoo <justin.yoo@aliencube.com>
2017-02-23 17:10:47 +1100

    Add AJAX call sample using axios

--- src/VueJsTypeScriptAspNetCoreSample/src/components/Hello.ts
+++ src/VueJsTypeScriptAspNetCoreSample/src/components/Hello.ts
@@ -1,17 +1,18 @@
 import * as Vue from "vue";
 import Component from "vue-class-component";
+import axios from "axios";
 
 @Component({
   name: "Hello",
 })
 export default class Hello extends Vue {
   msg: string = "Welcome to Your Vue.js App";
-//   created (): void {
-//     axios
-//         .get('/api/hello')
-//         .then((res) => {
-//           this.msg = res.data.message
-//         })
-//         .catch((ex) => console.log(ex))
-//   }
+  created (): void {
+    axios
+        .get("/api/hello")
+        .then((res) => {
+          this.msg = res.data.message;
+        })
+        .catch((ex) => console.log(ex))
+  }
 }
