a4a6a5abd1981d2430707f0ea29119fc2afdfeb6
Justin Yoo <justin.yoo@aliencube.com>
2017-03-02 15:16:38 +1100

    Update Hello.vue and Hello.ts for webcam

--- src/VueJsTsWebcamAspNetCoreSample/src/components/Hello.ts
+++ src/VueJsTsWebcamAspNetCoreSample/src/components/Hello.ts
@@ -6,13 +6,87 @@ import axios from "axios";
   name: "Hello",
 })
 export default class Hello extends Vue {
+  src: string = null;
+  width: string = "400";
+  height: string = "300";
+  autoplay: boolean = true;
+  photo: string = null;
+  screenshotFormat: string = "image/jpeg";
+
+  private _refs: { [key: string] : Vue | Element | Vue[] | Element[] } = null;
+  private _video: HTMLVideoElement = null;
+  private _stream: MediaStream = null;
+  private _hasUserMedia: boolean = false;
+  private _ctx: CanvasRenderingContext2D = null;
+
   msg: string = "Welcome to Your Vue.js App";
+
+  takePhoto (): void {
+    if (!this._hasUserMedia) {
+      return;
+    }
+
+    var canvas: HTMLCanvasElement = this.getCanvas();
+
+    this.photo = canvas.toDataURL(this.screenshotFormat);
+  }
+
+  getCanvas (): HTMLCanvasElement {
+    if (!this._hasUserMedia) {
+      return null;
+    }
+
+    if (this._ctx == null) {
+      var canvas: HTMLCanvasElement = document.createElement("canvas");
+      canvas.height = this._video.clientHeight;
+      canvas.width = this._video.clientWidth;
+
+      this._ctx = canvas.getContext("2d");
+
+      this._ctx.drawImage(this._video, 0, 0, canvas.width, canvas.height);
+
+      return canvas;
+    }
+
+    return null;
+  }
+
+  mounted (): void {
+    this._refs = this.$refs;
+    this._video = <HTMLVideoElement>this._refs["video"];
+
+    var n: any = <any>navigator;
+    n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia ||   n.msGetUserMedia || n.oGetUserMedia;
+
+    if (n.getUserMedia) {
+      n.getUserMedia({ video: true },
+        (stream) => {
+          this.src = window.URL.createObjectURL(stream);
+          this._stream = stream;
+          this._hasUserMedia = true;
+        },
+        (error) => {
+          console.log(error);
+      });
+    }
+  }
+
   created (): void {
     axios
-        .get("/api/hello")
-        .then((res) => {
-          this.msg = res.data.message;
-        })
-        .catch((ex) => console.log(ex));
+      .get("/api/hello")
+      .then((res) => {
+        this.msg = res.data.message;
+      })
+      .catch((ex) => console.log(ex));
+  }
+
+  beforeDestroy (): void {
+    this._video.pause();
+    this.src = "";
+    this._stream.getTracks()[0].stop();
+  }
+
+  destroyed (): void {
+    console.log("Destroyed");
   }
 }
--- src/VueJsTsWebcamAspNetCoreSample/src/components/Hello.vue
+++ src/VueJsTsWebcamAspNetCoreSample/src/components/Hello.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="hello">
+    <video v-bind:src="src" v-bind:width="width" v-bind:height="height" v-bind:autoplay="autoplay" ref="video"></video>
+    <hr/>
+    <img v-bind:src="photo" alt="" v-bind:width="width" v-bind:height="height"/>
+    <hr/>
+    <button type="button" v-on:click="takePhoto">Take Photo</button>
+    <hr/>
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
