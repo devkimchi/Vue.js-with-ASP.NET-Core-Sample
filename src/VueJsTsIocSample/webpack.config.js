90d9cae7a43c3b77870a70686933ecf9bdfe036d
Justin Yoo <justin.yoo@aliencube.com>
2017-03-18 21:39:33 +1100

    Add Vue.js + TypeScript IoC sample

---
+++ src/VueJsTsIocSample/webpack.config.js
@@ -0,0 +1,8 @@
+﻿"use strict";
+
+var isDevBuild = process.argv.indexOf('--env.prod') < 0;
+var devConfig = require("./build/webpack.dev.conf");
+var prodConfig = require("./build/webpack.prod.conf");
+
+module.exports = isDevBuild ? devConfig : prodConfig;
+
