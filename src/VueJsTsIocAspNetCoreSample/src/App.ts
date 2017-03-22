f8a759438c63ae030c20d9d91c73a83f298d7bea
Justin Yoo <justin.yoo@aliencube.com>
2017-03-22 16:17:48 +1100

    Update project to use IoC container

--- src/VueJsTsIocAspNetCoreSample/src/App.ts
+++ src/VueJsTsIocAspNetCoreSample/src/App.ts
@@ -1,8 +1,13 @@
 import Vue from "vue";
 import Component from "vue-class-component";
+import SERVICE_IDENTIFIER from "./models/Identifiers";
+import container from "./configs/DependencyConfigs";
 
 @Component({
-  name: "App"
+  name: "App",
+  provide: {
+    [SERVICE_IDENTIFIER.CONTAINER]: container
+  }
 })
 export default class App extends Vue {
 }
--- src/VueJsTsIocAspNetCoreSample/src/components/Ninja.ts
+++ src/VueJsTsIocAspNetCoreSample/src/components/Ninja.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
-import Component from "vue-class-component";
+import { Component, Inject } from "vue-property-decorator";
+import { Container } from "inversify";
 import SERVICE_IDENTIFIER from "../models/Identifiers";
-import container from "../configs/DependencyConfigs";
 import { Ninja as _Ninja } from "../models/Warrior";
 
 @Component({
@@ -11,10 +11,13 @@ export default class Ninja extends Vue {
   warrior: string;
   weapon: string;
 
+  @Inject(SERVICE_IDENTIFIER.CONTAINER)
+  private _container: Container;
+
   private _ninja: _Ninja;
 
   created (): void {
-    this._ninja = container.get<_Ninja>(SERVICE_IDENTIFIER.WARRIOR);
+    this._ninja = this._container.get<_Ninja>(SERVICE_IDENTIFIER.WARRIOR);
 
     this.warrior = this._ninja.name;
     this.weapon = this._ninja.weapon.name;
